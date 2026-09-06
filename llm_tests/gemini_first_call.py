aed429927de234f07fa66eb68ae79e2c5a00092b
Malik <workcmtiayon@gmail.com>
2026-09-06 09:55:46 +0100

    fix: correct Gemini content generation

--- llm_tests/gemini_first_call.py
+++ llm_tests/gemini_first_call.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 """Pour charger les variables d'environnement a partir du fichier .env"""
 load_dotenv()
@@ -9,7 +10,12 @@ client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
 
 response = client.models.generate_content(
     model="gemini-3.6-flash",
-    contents="Salut gar... donne moi un conseil sur comment etre un bon developpeur frontend dont la biblio principale est le REACT stp."
+    contents="Bonjour, ça va ?",
+    config=types.GenerateContentConfig(
+        automatic_function_calling=types.AutomaticFunctionCallingConfig(
+            disable=True
+        )
+    )
 )
 
 print(response.text)
